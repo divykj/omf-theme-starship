eabbdb00bd37d12893a66dab91e5f3351c010322
Divy Jain <dkj@somaiya.edu>
2021-01-23 17:19:23 +0530

    ✨ fix trucation on long prompt

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    starship init fish | source
+    source (starship init fish --print-full-init | sed 's/function fish_prompt/function fish_prompt_raw/' | psub)
     type -q neofetch
     and neofetch
     or echo (set_color green)(uname -npsr)(set_color normal)
