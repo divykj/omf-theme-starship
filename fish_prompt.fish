7356362c5e19149a57db1017cf2c21c8e39c775e
Divy Jain <dkj@somaiya.edu>
2021-01-23 19:13:01 +0530

    Fix truncation on long prompt (#1)

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,9 @@
 function fish_prompt
-    # Deliberately Empty
+    set -l raw_prompt (fish_prompt_raw | string split0)
+    set -l prompt_width (echo $raw_prompt | string replace -ra '\e\[[^m]*m|[^[:print:]]' '' | string length | sort -nr | head -1)
+    if test "$prompt_width" -ge "$COLUMNS"
+        echo $raw_prompt | string replace -ra '[[:space:]](?<!^)(?!$)' '\n'
+    else
+        echo $raw_prompt
+    end
 end
