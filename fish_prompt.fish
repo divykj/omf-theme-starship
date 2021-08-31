bae07ecddcf97ee3f16f177bcb6057d82d496542
Divy Jain <dkj@somaiya.edu>
2021-08-31 11:18:46 +0530

    new updates

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,9 +1,4 @@
 function fish_prompt
     set -l raw_prompt (fish_prompt_raw | string split0)
-    set -l prompt_width (echo $raw_prompt | string replace -ra '\e\[[^m]*m|[^[:print:]]' '' | string length | sort -nr | head -1)
-    if test "$prompt_width" -ge "$COLUMNS"
-        echo $raw_prompt | string replace -ra '[[:space:]](?<!^)(?!$)' '\n'
-    else
-        echo $raw_prompt
-    end
+    echo $raw_prompt
 end
